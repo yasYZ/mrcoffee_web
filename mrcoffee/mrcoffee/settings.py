70ca887250895cde6a6b1a537f0e5e0ef8e523b6
yasYZ <yasinyazdani71@gmail.com>
2023-12-30 17:10:09 +0330

    End

--- mrcoffee/mrcoffee/settings.py
+++ mrcoffee/mrcoffee/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(+9xq#s=-8+836$72&qo%n_2=8*=tlv^xt89494cmda7mlczg!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
